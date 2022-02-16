97640cfbc03c5305e7cd9a44c6e07846524f3ac6
bruno.poehlmann1 <poehlmann0110F>
2022-02-16 10:50:23 -0400

    actualizacion web

--- js/bcp.js
+++ js/bcp.js
@@ -1,34 +1,3 @@
-$(document).ready(function() {
-
-  var sync1 = $("#sync1");
-  var slidesPerPage = 3; //globaly define number of elements per page
-  var syncedSecondary = true;
-
-  sync1.owlCarousel({
-    items: 1,
-    slideSpeed: 2000,
-    nav: false,
-    autoplay: true,
-    dots: false,
-    loop:true,
-    autoplayTimeout:3000,
-    autoplayHoverPause:true,
-    responsiveRefreshRate: 200,
-  });
-
-  new WOW().init();
-
-  wow = new WOW(
-    {
-      boxClass:     'wow',      // default
-      animateClass: 'animated', // default
-      offset:       0,          // default
-      mobile:       true,       // default
-      live:         true        // default
-    }
-  )
-  wow.init();
-});
 $('#operacion_bcp').click(function(){
   Swal.fire({
     title: '<strong>Puedes realizar:</strong>',
@@ -58,7 +27,6 @@ $('#operacion_bcp').click(function(){
       '        <div class="timeline__event animated fadeInUp delay-2s timeline__event--type2">\n' +
       '          <div class="timeline__event__icon">\n' +
       '            💡\n' +
-      '\n' +
       '          </div>\n' +
       '          <div class="timeline__event__date">\n' +
       '            &nbsp;&nbsp;💡&nbsp;&nbsp;\n' +
@@ -86,13 +54,11 @@ $('#operacion_bcp').click(function(){
       '            <div class="timeline__event__description">\n' +
       '              <p><ul><li>Entre cuentas propias y a terceros</li><li>Interbancarias</li></ul></p>\n' +
       '            </div>\n' +
-      '\n' +
       '          </div>\n' +
       '        </div>\n' +
       '        <div class="timeline__event animated fadeInUp timeline__event--type1">\n' +
       '          <div class="timeline__event__icon">\n' +
       '            💳\n' +
-      '\n' +
       '          </div>\n' +
       '          <div class="timeline__event__date">\n' +
       '            &nbsp;&nbsp;💳&nbsp;&nbsp;\n' +
@@ -184,3 +150,230 @@ $(".check_srl").change(function(){
 });
 
 
+$('#beneficio_usar').click(function(){
+  Swal.fire({
+    title: '<h2><strong>¿Qué beneficios brindarias para tus clientes?</strong></h2>',
+    width: '45em',
+    confirmButtonColor: '#F26E29',
+    cancelButtonColor: '#E3E4E5',
+    confirmButtonText: 'Listo!',
+    html:
+      '        <div class="row justify-content-md-center">\n' +
+      '          <div class="col-md-12" >\n' +
+      '            <ol class="olcards">\n' +
+      '              <li class="wow fadeInLeft" data-wow-delay=".25s" style="--cardColor:#203864">\n' +
+      '                <div class="content">\n' +
+      '                  <div class="icon">🕔</div>\n' +
+      '                  <div class="title">Horario extendido</div>\n' +
+      '                  <div class="text">Atención en horarios extendidos en comparación con las oficinas del banco.</div>\n' +
+      '                </div>\n' +
+      '              </li>\n' +
+      '              <li class="wow fadeInLeft" data-wow-delay=".45s" style="--cardColor:rgba(0, 43, 118, 0.7)">\n' +
+      '                <div class="content">\n' +
+      '                  <div class="icon">📍</div>\n' +
+      '                  <div class="title">Más cerca de tus clientes</div>\n' +
+      '                  <div class="text">Te encontrarias en diferentes zonas de la ciudad, estando más cerca de tus clientes</div>\n' +
+      '                </div>\n' +
+      '              </li>\n' +
+      '              <li class="wow fadeInLeft" data-wow-delay=".65s" style="--cardColor:rgba(0, 43, 118, 0.4)">\n' +
+      '                <div class="content">\n' +
+      '                  <div class="icon">🚶‍♀</div>\n' +
+      '                  <div class="title">Sin colas de espera</div>\n' +
+      '                  <div class="text">El cliente no tiene que hacer colas o esperar mucho tiempo para ser atendido</div>\n' +
+      '                </div>\n' +
+      '              </li>\n' +
+      '            </ol>\n' +
+      '          </div>\n',
+    showCloseButton: true
+  })
+
+});
+
+$('#beneficio_ser').click(function(){
+  Swal.fire({
+    title: '<h2><strong>¿Qué beneficios tendrás como agente BCP?</strong></h2>',
+    width: '45em',
+    confirmButtonColor: '#F26E29',
+    cancelButtonColor: '#E3E4E5',
+    confirmButtonText: 'Listo!',
+    html:
+      '        <div class="row justify-content-md-center">\n' +
+      '          <div class="col-md-12" >\n' +
+      '             <ol class="olcards">\n' +
+      '              <li class="wow fadeInRight" data-wow-delay=".25s" style="--cardColor:#2B9BE0">\n' +
+      '                <div class="content">\n' +
+      '                  <div class="icon">📈</div>\n' +
+      '                  <div class="title">Mayor productividad</div>\n' +
+      '                  <div class="text">Vuelves mas productivo el tiempo sin actividad en tu negocio</div>\n' +
+      '                </div>\n' +
+      '              </li>\n' +
+      '              <li class="wow fadeInRight" data-wow-delay=".45s" style="--cardColor:rgba(43, 155, 224, 0.7)">\n' +
+      '                <div class="content">\n' +
+      '                  <div class="icon">👥</div>\n' +
+      '                  <div class="title">Incremento de clientes</div>\n' +
+      '                  <div class="text">Generarás una mayor cantidad de clientes, lo que significa oportunidad de subir tus ventas</div>\n' +
+      '                </div>\n' +
+      '              </li>\n' +
+      '              <li class="wow fadeInRight" data-wow-delay=".65s" style="--cardColor:rgba(43, 155, 224, 0.5)">\n' +
+      '                <div class="content">\n' +
+      '                  <div class="icon">🗣️</div>\n' +
+      '                  <div class="title">Clientes concurrentes</div>\n' +
+      '                  <div class="text">Tus clientes volverán ya que verán la comodidad de pagar a travéz de tu negocio</div>\n' +
+      '                </div>\n' +
+      '              </li>\n' +
+      '              <li class="wow fadeInRight" data-wow-delay=".85s" style="--cardColor:rgba(43, 155, 224, 0.3)">\n' +
+      '                <div class="content">\n' +
+      '                  <div class="icon">➕</div>\n' +
+      '                  <div class="title">Mayor gama de servicios</div>\n' +
+      '                  <div class="text">Brindarás una mayor gama de servicios que los demás negocios</div>\n' +
+      '                </div>\n' +
+      '              </li>\n' +
+      '            </ol>'+
+      '          </div>\n',
+    showCloseButton: true
+  })
+
+});
+
+$('#requisitos_agente_bcp').click(function(){
+  Swal.fire({
+    title: '<h2>Revisa si cumples con los requisitos para ser Agente BCP</h2>',
+    width: '75%',
+    confirmButtonColor: '#F26E29',
+    cancelButtonColor: '#E3E4E5',
+    confirmButtonText: 'Listo!',
+    html:
+      '        <svg viewBox="0 0 0 0" style="position: absolute; z-index: -1; opacity: 0;">\n' +
+      '          <defs>\n' +
+      '            <linearGradient id="boxGradient" gradientUnits="userSpaceOnUse" x1="0" y1="0" x2="25" y2="25">\n' +
+      '              <stop offset="0%"   stop-color="#27FDC7"/>\n' +
+      '              <stop offset="100%" stop-color="#0FC0F5"/>\n' +
+      '            </linearGradient>\n' +
+      '            <linearGradient id="lineGradient">\n' +
+      '              <stop offset="0%"    stop-color="#0FC0F5"/>\n' +
+      '              <stop offset="100%"  stop-color="#27FDC7"/>\n' +
+      '            </linearGradient>\n' +
+      '            <path id="todo__line" stroke="url(#lineGradient)" d="M21 12.3h168v0.1z"></path>\n' +
+      '            <path id="todo__box" stroke="url(#boxGradient)" d="M21 12.7v5c0 1.3-1 2.3-2.3 2.3H8.3C7 20 6 19 6 17.7V7.3C6 6 7 5 8.3 5h10.4C20 5 21 6 21 7.3v5.4"></path>\n' +
+      '            <path id="todo__check" stroke="url(#boxGradient)" d="M10 13l2 2 5-5"></path>\n' +
+      '            <circle id="todo__circle" cx="13.5" cy="12.5" r="10"></circle>\n' +
+      '          </defs>\n' +
+      '        </svg>\n' +
+      '        <div class="row justify-content-md-center">\n' +
+      '          <div class="col-md-auto " >\n' +
+      '            <h3 class="wow fadeInLeft text-center pd-l-r">Requisitos para negocios propios</h3>\n' +
+      '            <h4 class="wow fadeInLeft pd-l-r">Cualquier tipo de comercio que tenga <br>antiguedad de negocio puede ser agente BCP</h4>\n' +
+      '            <div class="todo-list">\n' +
+      '              <label class="todo wow fadeInLeft" data-wow-delay=".25s">\n' +
+      '                <input class="todo__state check_propio "  type="checkbox" />\n' +
+      '                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 200 25" class="todo__icon " >\n' +
+      '                  <use xlink:href="#todo__line" class="todo__line"></use>\n' +
+      '                  <use xlink:href="#todo__box" class="todo__box"></use>\n' +
+      '                  <use xlink:href="#todo__check" class="todo__check"></use>\n' +
+      '                  <use xlink:href="#todo__circle" class="todo__circle"></use>\n' +
+      '                </svg>\n' +
+      '                <div class="todo__text " >Fotocopia de CI vigente</div>\n' +
+      '              </label>\n' +
+      '              <label class="todo wow fadeInLeft" data-wow-delay=".45s">\n' +
+      '                <input class="todo__state check_propio" type="checkbox" />\n' +
+      '                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 200 25" class="todo__icon">\n' +
+      '                  <use xlink:href="#todo__line" class="todo__line"></use>\n' +
+      '                  <use xlink:href="#todo__box" class="todo__box"></use>\n' +
+      '                  <use xlink:href="#todo__check" class="todo__check"></use>\n' +
+      '                  <use xlink:href="#todo__circle" class="todo__circle"></use>\n' +
+      '                </svg>\n' +
+      '                <div class="todo__text">Licencia de funcionamiento</div>\n' +
+      '              </label>\n' +
+      '              <label class="todo wow fadeInLeft" data-wow-delay=".65s">\n' +
+      '                <input class="todo__state check_propio" type="checkbox" />\n' +
+      '                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 200 25" class="todo__icon">\n' +
+      '                  <use xlink:href="#todo__line" class="todo__line"></use>\n' +
+      '                  <use xlink:href="#todo__box" class="todo__box"></use>\n' +
+      '                  <use xlink:href="#todo__check" class="todo__check"></use>\n' +
+      '                  <use xlink:href="#todo__circle" class="todo__circle"></use>\n' +
+      '                </svg>\n' +
+      '                <div class="todo__text">NIT</div>\n' +
+      '              </label>\n' +
+      '              <label class="todo wow fadeInLeft" data-wow-delay=".85s">\n' +
+      '                <input class="todo__state check_propio" type="checkbox" />\n' +
+      '                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 200 25" class="todo__icon">\n' +
+      '                  <use xlink:href="#todo__line" class="todo__line"></use>\n' +
+      '                  <use xlink:href="#todo__box" class="todo__box"></use>\n' +
+      '                  <use xlink:href="#todo__check" class="todo__check"></use>\n' +
+      '                  <use xlink:href="#todo__circle" class="todo__circle"></use>\n' +
+      '                </svg>\n' +
+      '                <div class="todo__text">Papeles del inmueble donde funciona el negocio:\n' +
+      '                                        <ul>\n' +
+      '                                          <li>Fotocopia de Contrato Alquiler o Anticrético</li>\n' +
+      '                                          <li>Fotocopia de Folio Real (si es propio)</li>\n' +
+      '                                        </ul>\n' +
+      '                </div>\n' +
+      '              </label>\n' +
+      '              <label class="todo wow fadeInLeft" data-wow-delay="1.05s">\n' +
+      '                <input class="todo__state check_propio" type="checkbox" />\n' +
+      '                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 200 25" class="todo__icon">\n' +
+      '                  <use xlink:href="#todo__line" class="todo__line"></use>\n' +
+      '                  <use xlink:href="#todo__box" class="todo__box"></use>\n' +
+      '                  <use xlink:href="#todo__check" class="todo__check"></use>\n' +
+      '                  <use xlink:href="#todo__circle" class="todo__circle"></use>\n' +
+      '                </svg>\n' +
+      '                <div class="todo__text">Antecedentes Policiales y Judiciales</div>\n' +
+      '              </label>\n' +
+      '              <label class="todo wow fadeInLeft" data-wow-delay="1.25s">\n' +
+      '                <input class="todo__state check_propio" type="checkbox" />\n' +
+      '                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 200 25" class="todo__icon">\n' +
+      '                  <use xlink:href="#todo__line" class="todo__line"></use>\n' +
+      '                  <use xlink:href="#todo__box" class="todo__box"></use>\n' +
+      '                  <use xlink:href="#todo__check" class="todo__check"></use>\n' +
+      '                  <use xlink:href="#todo__circle" class="todo__circle"></use>\n' +
+      '                </svg>\n' +
+      '                <div class="todo__text">Resolución de SEDES (solo Farmacias o afines a Salud)</div>\n' +
+      '              </label>\n' +
+      '            </div>\n' +
+      '          </div>\n' +
+      '          <div class="col-md-auto">\n' +
+      '            <h3  class="wow fadeInRight text-center pd-l-r">Requisitos para negocios SRL o SA</h3>\n' +
+      '            <h4 class="wow fadeInRight pd-l-r">Además de los requisitos ya planteados, debes proporcionarnos:<br><br></h4>\n' +
+      '            <div class="todo-list">\n' +
+      '              <label class="todo wow fadeInRight" data-wow-delay=".25s">\n' +
+      '                <input class="todo__state check_srl" type="checkbox" />\n' +
+      '                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 200 25" class="todo__icon">\n' +
+      '                  <use xlink:href="#todo__line" class="todo__line"></use>\n' +
+      '                  <use xlink:href="#todo__box" class="todo__box"></use>\n' +
+      '                  <use xlink:href="#todo__check" class="todo__check"></use>\n' +
+      '                  <use xlink:href="#todo__circle" class="todo__circle"></use>\n' +
+      '                </svg>\n' +
+      '                <div class="todo__text">Poder del representante legal</div>\n' +
+      '              </label>\n' +
+      '              <label class="todo wow fadeInRight" data-wow-delay=".45s">\n' +
+      '                <input class="todo__state check_srl" type="checkbox" />\n' +
+      '                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 200 25" class="todo__icon">\n' +
+      '                  <use xlink:href="#todo__line" class="todo__line"></use>\n' +
+      '                  <use xlink:href="#todo__box" class="todo__box"></use>\n' +
+      '                  <use xlink:href="#todo__check" class="todo__check"></use>\n' +
+      '                  <use xlink:href="#todo__circle" class="todo__circle"></use>\n' +
+      '                </svg>\n' +
+      '                <div class="todo__text">Registro de Comercio</div>\n' +
+      '              </label>\n' +
+      '            </div>\n' +
+      '          </div>\n' +
+      '        </div>\n' +
+      '        <h5 style="margin-top: 20px;text-align: center;font-size: 19px"><b>Nota: Debes contar con una conexión a internet</b></h5>\n',
+    showCloseButton: true
+  })
+
+});
+
+$('#agentes-cercanos').click(function(){
+  Swal.fire({
+    title: '<h2><strong>Agentes BCP cercanos a tí</strong></h2>',
+    width: '45em',
+    confirmButtonColor: '#F26E29',
+    cancelButtonColor: '#E3E4E5',
+    confirmButtonText: 'Listo!',
+    html:
+      '<iframe src="https://www.google.com/maps/d/u/1/embed?mid=1pMd3NZdrUhqfGps3qEePFRPeVEcXgn6u&ehbc=2E312F" width="640" height="480"></iframe>',
+    showCloseButton: true
+  })
+
+});
